02deac558634f5ac5cd71a880287b6e1facff294
Sandesh <kandel.sandesh@outlook.com>
2020-06-27 19:55:57 +1000

    automatic 2048 player with selenium

---
+++ xcpd.py
@@ -0,0 +1,35 @@
+#Scraping Images from the XKCD comic site
+
+import requests, os, bs4
+url="https://xkcd.com"
+os.makedirs('xkcd', exist_ok=True) #store images in ./xkcd
+while not url.endswith('#'):
+    #Download the page
+    print('Downloading the page %s...' %url)
+    res= requests.get(url)
+    res.raise_for_status()
+    soup= bs4.BeautifulSoup(res.text, 'html.parser')
+
+    #find URL of the image
+    comicElem= soup.select('#comic img')
+    if comicElem==[]:
+        print('Could not find comic images')
+    else:
+        comicUrl='https:'+comicElem[0].get('src')
+        #Download the image
+
+        print('Downloading image %s...' %(comicUrl))
+        res=requests.get(comicUrl)
+        res.raise_for_status()
+
+        #save image to ./xkcd
+        imageFile= open(os.path.join('xkcd', os.path.basename(comicUrl)),'wb')
+        for chunk in res.iter_content(10000):
+            imageFile.write(chunk)
+        imageFile.close()
+        #get prev button's url
+        prevLink=soup.select('a[rel="prev"]')[0]
+        url='https://xkcd.com' +prevLink.get('href')
+print('Done')
+
+
